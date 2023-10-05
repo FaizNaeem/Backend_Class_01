8027a0a12e5eb2c0ddff52984c7c7d9d0863dc19
FaizNaeem <faiznaeem192022@gmail.com>
2023-10-05 19:59:51 +0500

    Express js

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ app.get("/hi", (req ,res)=>{
 app.listen(3000,()=>{
     console.log("server ready" + 3000);
 })
+   
+
+
+// some step use node js 
+// / asynchronous / non-blocking 
+// synchronous  / blocking 
+// Request Fucntion / Get / Put / Patch / Post / Delete
+//command 
+// step 01 --> : npm init
+// step 02 --> : npm i express
+// step 03 -->: npm i nodemon -g
+// step 04 -->: Run Terminal / node index.js
+// step 05 -->: Run Terminal / nodemon index.js
+// step 06 --> : Install Thunder Client Extention And Go to Thander Client and click new Request and paste url 
