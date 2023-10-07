5b3a9afbcb016990f9854fb8812a91b62adfcd3e
Faiz Naeem <81919141+FaizNaeem@users.noreply.github.com>
2023-10-07 19:23:24 +0500

    Backend

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 const express = require("express")
 var app = express()
+const user = require("./user");
+const morgan = require("morgan");
+
+
+const middelever=(req ,res , next)=>{
+console.log("Middlewere console");
+next()
+}
+app.use(middelever)
 app.get("/hi", (req ,res)=>{
     res.send("Server ready to Go hello")
-// req.send()
+
 })
+app.use('/user', user)
+app.use(morgan("tiny"))
+// get user id using params 
+// app.get('/user/:id', (req, res) => {
+//     console.log('console.log-->', req.params.id)
+//     console.log('console.log-->', req.query)
+//     res.send({ 
+//         status: 200,
+//         msg: "User id is " + req.params.id
+//     })
+// })
+app.get("/about", (req ,res)=>{
+    res.send({
+        status :200,
+        massage :"About working"
+    })
+    
+
+})
+
+
+
+
+
+
+
+
+
+
+
 app.listen(3000,()=>{
     console.log("server ready" + 3000);
 })
---
+++ user.js
@@ -0,0 +1,49 @@
+const app = require("express")
+const router = app.Router()
+const user = [
+    {
+    Name:"faiz",
+    id:1
+},
+{
+    Name:"Junaid",
+    id:2
+},
+{
+    Name:"Atta",
+    id:3
+},
+{
+    Name:"Bilal",
+    id:4
+},
+]
+router.get('/',(req, res)=>{
+res.status(200).send({
+    status:200,
+    massage:"All users",
+    Users:user
+})
+// console.log(user);
+})
+router.get('/:id',(req, res)=>{
+    let user1 = user.find(e=>e.id==req.params.id)
+    if(user1){
+        res.status(200).send({
+            user:user1,
+            Meassage :"User Fonuded"
+
+        })
+    }
+    else{
+        res.status(500).send(
+            {
+                status:500,
+                Error:true,
+                massage:"User Not Found"
+                
+        }
+        )
+    }
+})
+module.exports= router
